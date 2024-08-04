6c2975983f236cb6785595e87437503af7335b18
Dildar05 <noraacc09@gmail.com>
2024-08-05 03:05:01 +0500

    dark mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,25 @@
-document.querySelector('.header__dark-mode').addEventListener('click', function () {
-  const move = document.querySelector('.move');
-  move.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', (event) => {
+  //darkMode
+  const currentTheme = localStorage.getItem('theme');
+  const darkModeButton = document.querySelector('.header__dark-mode');
+
+  if (currentTheme === 'light') {
+    darkModeButton.classList.add('active');
+  } else {
+    darkModeButton.classList.remove('active');
+  }
+
+  darkModeButton.addEventListener('click', () => {
+    const rootElement = document.documentElement;
+    let dataTheme = rootElement.getAttribute('data-theme'),
+      newTheme;
+
+    newTheme = dataTheme === 'light' ? 'dark' : 'light';
+    rootElement.setAttribute('data-Theme', newTheme);
+
+    localStorage.setItem('theme', newTheme);
+
+    darkModeButton.classList.toggle('active');
+  });
+  //next functions
 });
