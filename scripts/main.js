a55047377a037b4a363adc11831ecb85e8c5812f
Dildar05 <noraacc09@gmail.com>
2024-09-01 14:48:38 +0500

    script

--- scripts/main.js
+++ scripts/main.js
@@ -132,11 +132,11 @@ if (popupBtn && popup && popupBody) {
     });
   });
 
-  // popup.addEventListener('click', (e) => {
-  //   if (e.target === popupBody) {
-  //     popup.classList.remove('open');
-  //   }
-  // });
+  popup.addEventListener('click', (e) => {
+    if (e.target === popupBody) {
+      popup.classList.remove('open');
+    }
+  });
 
   document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
@@ -144,7 +144,46 @@ if (popupBtn && popup && popupBody) {
     }
   });
 }
+const form = document.querySelector('.contacts__form');
+const result = document.getElementById('result');
 
+form.addEventListener('submit', function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+
+  result.innerHTML = 'Please wait...';
+
+  fetch('https://api.web3forms.com/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = 'Something went wrong!';
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = 'none';
+      }, 3000);
+    });
+});
 // document.addEventListener('DOMContentLoaded', () => {
 //   // Темный режим
 
